6937e88ceec5b3b918c9684bc0bb8792dc51fbff
marcosrodriigues <marcos.rodriiigues@gmail.com>
2020-11-21 14:41:06 -0300

    finish release

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Feather } from '@expo/vector-icons';
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
